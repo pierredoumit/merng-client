1712414e90f5e3811399831d0e85c7b2628953e9
Michel ALAKL <al.akl.michel@gmail.com>
2021-01-28 15:00:33 +0200

    deploy

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNblVEdHY-BoY0T9BEpyRmvtgT1snDSQA",
+    authDomain: "merng-graphql.firebaseapp.com",
+    projectId: "merng-graphql",
+    storageBucket: "merng-graphql.appspot.com",
+    messagingSenderId: "552230277449",
+    appId: "1:552230277449:web:634a7b40148693281d8a98"
+};
